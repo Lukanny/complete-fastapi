5261a439095210a40e9294a3d05791b2341bca9e
vinny <vini@vinny>
2024-10-29 18:43:27 -0400

    updating tests and adding shcemas.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,11 +5,10 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-
+def test_root_deve_retornar_ola_mundo_em_html():
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Hello': 'World'}
+    assert '<h1> Olá Mundo </h1>' in response.text
